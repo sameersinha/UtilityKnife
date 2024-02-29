87715828dbdc3a2078ae576c1b7a47e63c2d87eb
Sameer Sinha <sameersinha08@gmail.com>
2024-02-29 09:09:46 +0530

    pull for latest changes

---
+++ Converters/FahrenheitToCelsius.cs
@@ -0,0 +1,9 @@
+namespace UtilityKnife.Converters {
+    public class FahrenheitToCelsius {
+        public double FahrenheitToCelsiusConverter(double FahrenheitTemp) {
+            double _fahrenheitTemp = 0.0;
+            double _celsius = 0.0;
+            return _celsius;
+        }
+    }
+}
