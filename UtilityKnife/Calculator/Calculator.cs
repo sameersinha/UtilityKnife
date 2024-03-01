8c8872b25820347856fe7aaf7f56d1935681e59c
Sameer Sinha <sameersinha08@gmail.com>
2024-03-01 09:06:22 +0530

    added subtract method to calculator

--- UtilityKnife/Calculator/Calculator.cs
+++ UtilityKnife/Calculator/Calculator.cs
@@ -3,5 +3,9 @@ namespace UtilityKnife.Calculator {
         public static int Add(int x, int y) {
             return x + y;
         }
+
+        public static int Subtract(int x, int y) {
+            return x - y;
+        }
     }
 }
